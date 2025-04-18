925c383e48d433727cc9f18586e71742cfb7c603
garg.sid6665@gmail.com <sidg@Siddharth-ka-laipatopa.local>
2025-04-18 12:47:08 +0530

    fix: change to post

--- src/apis/featureApis.ts
+++ src/apis/featureApis.ts
@@ -3,7 +3,7 @@ import { aesDecrypt } from '../utility/index.utility';
 
 export class FeatureApis {
     static async getFeatureList(referenceid: string) {
-        const data = await axios.get(
+        const data = await axios.post(
             `https://routes.msg91.com/api/${referenceid}/widget`
             )
         return aesDecrypt(data?.data?.data?.ciphered)
