11f4fd86b8387dbdab3df30663b1e310b26484be
Rudraksh <rudrakshkachhawa@gmail.com>
2025-08-05 16:50:10 +0530

    Use decrypted features

--- src/apis/featureApis.ts
+++ src/apis/featureApis.ts
@@ -1,30 +1,29 @@
 import axios from 'axios';
-import { aesDecrypt } from '../utility/index.utility';
 
 export class FeatureApis {
     static async getFeatureList(referenceid: string) {
         const data = await axios.post(
-            `https://routes.msg91.com/api/${referenceid}/widget`
+            `https://routes.msg91.com/api/${referenceid}/widget?source=mobileSDK`
         )
-        return aesDecrypt(data?.data?.data?.ciphered)
+        return data.data?.data
     }
     static async getProxyAuthTokenForGoogleAuth(state: string, idToken: string) {
-        try{
+        try {
             const data = await axios.post(
                 `https://routes.msg91.com/api/auth/callback?source=mobileSDK&state=${state}&accessToken=${idToken}`
             )
             return data.data
-        }catch(e){
+        } catch (e) {
             throw e
         }
     }
-    static async getProxyAuthTokenForAppleAuth(state: string, idToken: string,authorizationCode:string) {
-        try{
+    static async getProxyAuthTokenForAppleAuth(state: string, idToken: string, authorizationCode: string) {
+        try {
             const data = await axios.post(
                 `https://routes.msg91.com/api/auth/callback?source=mobileSDK&state=${state}&accessToken=${idToken}&id_token=${idToken}&code=${authorizationCode}`
             )
             return data.data
-        }catch(e){
+        } catch (e) {
             throw e
         }
     }
